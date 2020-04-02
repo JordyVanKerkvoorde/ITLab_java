3caa6867331546f50874215ccf5d875588bec755
Sander Machado <sander.castanheiramachado@student.hogent.be>
2020-04-02 20:19:04 +0200

    fixed fxml

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,5 +1,6 @@
 package sample;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -9,17 +10,21 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.util.Objects;
+
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage){
-        HBox box = new HBox();
-        Label label = new Label("Test");        // this line throws the InvocationTargetException
-        box.getChildren().add(label);
-        Scene scene = new Scene(box);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("sample.fxml")));
+
+        Scene scene = new Scene(root, 300, 275);
+
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
