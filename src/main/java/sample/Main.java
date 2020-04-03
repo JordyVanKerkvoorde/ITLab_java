7a11f15ae68eaff8c3e6717334147d7b22bec169
Sander Machado <sander.castanheiramachado@student.hogent.be>
2020-04-03 18:29:44 +0200

    changed images

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.event.MouseEvent;
 import java.util.Objects;
 
 public class Main extends Application {
--- src/main/java/sample/controller/MainController.java
+++ src/main/java/sample/controller/MainController.java
@@ -12,9 +12,11 @@ import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.animation.FadeTransition;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
@@ -54,12 +56,11 @@ public class MainController implements Initializable, Callback {
             Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        HamburgerBackArrowBasicTransition transition = new HamburgerBackArrowBasicTransition(hamburger);
-        transition.setRate(1);
+        var transition = new HamburgerBackArrowBasicTransition(hamburger);
+        transition.setRate(-1);
         hamburger.addEventHandler(MouseEvent.MOUSE_PRESSED, (e) -> {
             transition.setRate(transition.getRate() * -1);
             transition.play();
-
             if (drawer.isOpened()) {
                 drawer.close();
             } else {
@@ -83,12 +84,12 @@ public class MainController implements Initializable, Callback {
             StackPane pane = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("views/splash.fxml")));
             root.getChildren().setAll(pane);
 
-            FadeTransition fadeIn = new FadeTransition(Duration.seconds(1), pane);
+            FadeTransition fadeIn = new FadeTransition(Duration.seconds(1.5), pane);
             fadeIn.setFromValue(0);
             fadeIn.setToValue(1);
             fadeIn.setCycleCount(1);
 
-            FadeTransition fadeOut = new FadeTransition(Duration.seconds(1), pane);
+            FadeTransition fadeOut = new FadeTransition(Duration.seconds(1.5), pane);
             fadeOut.setFromValue(1);
             fadeOut.setToValue(0);
             fadeOut.setCycleCount(1);
