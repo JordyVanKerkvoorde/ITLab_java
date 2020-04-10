d1fc4f025f8f6ea9d1c30ca59a917b09c6036484
Jan Willem <janwill1996@gmail.com>
2020-04-10 16:38:24 +0200

    database connection config

--- src/main/java/domain/Session.java
+++ src/main/java/domain/Session.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
+@NamedQuery(name = "Session.findAll", query="Select s from Session s")
 @Table(name = "Session")
 public class Session {
 
