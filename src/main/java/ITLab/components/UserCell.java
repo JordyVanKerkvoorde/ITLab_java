64581c5b6751896c5c80d173c615e716d83256d0
VandeWoestyneYorick <yorick.vandewoestyne@student.hogent.be>
2020-05-02 14:32:30 +0200

    Add boilerplate for UserView

---
+++ src/main/java/ITLab/components/UserCell.java
@@ -0,0 +1,12 @@
+package ITLab.components;
+
+import com.jfoenix.controls.JFXListCell;
+import domain.model.user.User;
+
+public class UserCell extends JFXListCell<User> {
+
+    @Override
+    protected void updateItem(User item, boolean empty) {
+        super.updateItem(item, empty);
+    }
+}
--- src/main/java/ITLab/controller/UserPopoverController.java
+++ src/main/java/ITLab/controller/UserPopoverController.java
@@ -5,7 +5,7 @@ import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class UsersViewController implements Initializable {
+public class UserPopoverController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
